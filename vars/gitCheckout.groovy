06a0b75da68dd63e2812d4d71a375d8b5ecc30a5
Adrija Basu <adrija.tina93@gmail.com>
2024-01-25 23:14:19 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -2,7 +2,7 @@ def call(Map stageParams)
 {
     checkout{[
         $class: 'GitSCM'
-        branches: [[ name: stageparams.branch ]]
-        userRemoteConfigs: [[ url: stageparams.url]]
+        branches: [[ name: stageParams.branch ]]
+        userRemoteConfigs: [[ url: stageParams.url]]
     ]}
 }
