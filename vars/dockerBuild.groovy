fd709a6dee56f3073ed25063b06398ada898c799
adrijatina2016 <adrija.tina93@gmail.com>
2024-01-26 18:31:28 +0530

    New changes committed
    Signed-off-by: adrijatina2016 <adrija.tina93@gmail.com>

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,7 @@
-def call ()
-{
+def call(String aws_account_id, String region, String ecr_repoName){
+    
     sh """
-    docker build -t springboot:latest .
-    docker save -o springboot.tar springboot:latest
+     docker build -t ${ecr_repoName} .
+     docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
